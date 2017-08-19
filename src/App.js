197dffb6ee08644efecd30349606370a28df78a4
Yiran Sheng <shengyiran@gmail.com>
2017-08-18 21:16:53 -0700

    render it

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Provider } from "react-redux";
 import logger from 'redux-logger'
 import logo from './logo.svg';
 import './App.css';
+import Thread from "./containers/Thread";
 
 import rootReducer from "./reducers";
 
@@ -18,10 +19,7 @@ class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
+        <Thread />
       </Provider>
     );
   }
