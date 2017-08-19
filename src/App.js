ce9c1abb90d163c073ff3564c8bd8e6104920e65
Yiran Sheng <shengyiran@gmail.com>
2017-08-18 23:26:43 -0700

    remove defaults

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
-import logger from 'redux-logger'
-import logo from './logo.svg';
-import './App.css';
+import logger from "redux-logger";
 import Thread from "./containers/Thread";
 
 import rootReducer from "./reducers";
 
-const store = createStore(
-  rootReducer,
-  undefined,
-  applyMiddleware(logger)
-);
+const store = createStore(rootReducer, undefined, applyMiddleware(logger));
 window.store = store;
 
 class App extends Component {
